179bc296b3b876348a85a2142ac80502601ec33b
milestag <primero.tag@gmail.com>
2019-04-26 16:37:15 +0300

    Initial commit

---
+++ WebApplication5/Migrations/20190306161726_init2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication5.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SecondName",
+                table: "Student",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SecondName",
+                table: "Student");
+        }
+    }
+}
